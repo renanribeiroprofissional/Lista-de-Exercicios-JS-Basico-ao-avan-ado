19342cc7660378b0f5c989162c6203be2d7d269d
renanribeiroprofissional <renanprogramadorprofisional@gmail.com>
2024-12-31 00:23:48 -0300

    adicionando exercicios

---
+++ If e Else/exercicio4.js
@@ -0,0 +1,15 @@
+// Exercício 4:
+
+// Crie um programa que leia um número e verifique se ele é positivo, negativo ou zero. Exiba a mensagem correspondente.
+
+let numero = 0;
+
+if (numero > 0) {
+  console.log("Positivo");
+} else if (numero < 0) {
+  console.log("Negativo");
+} else if (numero === 0) {
+  console.log("Número zero");
+} else {
+  console.log("Obrigado...");
+}
---
+++ If e Else/exercicio5.js
@@ -0,0 +1,11 @@
+// Exercício 5:
+
+// Peça ao usuário para digitar a sua senha. O programa deve verificar se a senha digitada é "1234". Se for, exiba "Senha correta", caso contrário, exiba "Senha incorreta".
+
+const senha = 1234;
+
+if (senha === 1234) {
+  console.log("Senha correta ");
+} else {
+  console.log("Senha incorreta ");
+}
